3aed1eaff9a7ca8d29bece8e2758de41a8969101
Kunal Bhalla <bhalla.kunal@gmail.com>
2011-06-12 18:08:12 +0530

    Moved constants to a separate file.

---
+++ pt-constants.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Define universal constants. 
+ * @package PressTest
+ */
+
+/** Plugin Directory */
+define( 'PT_DIR', WP_PLUGIN_DIR . '/presstest' );
+/** Plugin URL */
+define( 'PT_URL', plugins_url( 'presstest' ) );
+/** Plugin Homepage */
+define( 'PT_HOME', 'http://github.com/kunalb/presstest/' );
+/** PressTest Admin Slug */
+define( 'PT_SLUG', 'presstest' );
