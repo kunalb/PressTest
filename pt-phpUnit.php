22f9cd43ccd43862a1b553e6b899aa645a446596
Kunal Bhalla <bhalla.kunal@gmail.com>
2011-09-07 01:35:17 +0530

    Runs the phpUnit tests.
    Basic harness to actually run the phpUnit tests -- does not
    load tests.

---
+++ pt-phpUnit.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Loads and run the phpUnit tests for the 
+ * plugin specified in the GET parameters.
+ *
+ * Does not include any WordPress files --
+ * any tests must include them as required.
+ *
+ * @package PressTest
+ * @author Kunal Bhalla
+ * @version 0.1
+ */
+
+ 
