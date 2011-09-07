2aeac260edf5a7bfa82ac33434443bff6b592bdc
Kunal Bhalla <bhalla.kunal@gmail.com>
2011-09-07 13:50:29 +0530

    Added valid tests to test the tester.

--- ptests/sampletest.js
+++ ptests/sampletest.js
@@ -1,1 +1,24 @@
-"use strict";
+jQuery(document).ready(function(){
+	test( "A basic test example", function(){
+		ok( true, "Passed test" );
+	});
+
+	module("Module A");
+
+	test("first test within module", function() {
+	  ok( true, "all pass" );
+	});
+
+	test("second test within module", function() {
+	  ok( true, "all pass" );
+	});
+
+	module("Module B");
+
+	test("some other test", function() {
+	  expect(2);
+	  equal( true, false, "failing test" );
+	  equal( true, true, "passing test" );
+	});
+
+});
