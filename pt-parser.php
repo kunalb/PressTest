2b46bae35d96dae588ed8bb0401dafaf575204c6
Kunal Bhalla <bhalla.kunal@gmail.com>
2011-09-21 03:21:20 +0530

    Add saving function for modifier

--- pt-parser.php
+++ pt-parser.php
@@ -8,7 +8,8 @@ abstract class PT_Parse {
 	private static $position;
 	private static $tokens_length;
 	private static $mods;
-	private static $mod_list = Array( 
+	private static $mod_distance;
+	protected static $mod_list = Array( 
 		T_DOC_COMMENT,
 		T_FINAL,
 		T_PUBLIC,
@@ -25,54 +26,72 @@ abstract class PT_Parse {
 		$this->postition = $this->tokens_length();
 	}
 
+	protected function end() {
+		if( $this->position < $this->tokens_length ) 
+			return false;
+		return true;	
+	}
+
 	protected function reset() {
 		$this->position = 0;
 	}
 
 	protected function set( $tokens ) {
-		
 		$this->postition = 0;
 		$this->tokens = $tokens;
 		$this->tokens_length = count( $tokens );
 		$this->mods = Array();
-
 	}
 
 	private function modifiers() {
-		$modifier_list = Array( 
+		if( in_array( $this->token(), $this->mod_list ) ) {
+			$this->mods[ $this->token() ] = $this->value();
+			$this->mod_distance = 0;
+		} else if( $this->token() != T_WHITESPACE ) {
+			$this->mod_distance++;
+			if( $this->mod_distance > 0 )
+				$this->reset_modifiers();
+		}
 	}
 
 	private function reset_modifiers() {
+		$this->mods = Array();
 	}
 
 	protected function get_modifier( $token ) {
+		return $this->mods[ $this->token() ];
+	}
 
+	protected function each() {
+		$this->modifiers();
 	}
 
 	protected function next() {
 		$this->position++;
-
-		$this->modifiers();
-	
-		if( $this->position < $this->tokens_length ) 
-			return true;
-		
-		return false;
+		if( !$this->end() )
+			$this->each();
+		return !$this->end();
 	}
 
 	protected function token() {
+		if( $this->end() ) return false;
+
 		if( is_array( $this->tokens[ $this->position ] ) )
 			return $this->tokens[ $this->position ][ 0 ];
 		else return $this->tokens[ $this->position ];
 	}	
 
 	protected function value() {
+		if( $this->end() ) return false;
+
 		if( is_array( $this->tokens[ $this->position ] ) )
 			return $this->tokens[ $this->position ][ 1 ];
 		else return $this->tokens[ $this->position ];
 	}	
 
 	protected function line() {
+		if( $this->end() ) return false;
+
 		if( is_array( $this->tokens[ $this->position ] ) && isset( $this->tokens[ $this->position ][ 2 ] ) ) 
 			return $this->tokens[ $this->position ][ 2 ];
 	
@@ -80,7 +99,7 @@ abstract class PT_Parse {
 	}	
 
 	protected function skip_till( $token ) {
-		while( $this->token != $token )
+		while( $this->token() != $token && !$this->end() )
 			$this->next();
 	}
 
@@ -89,15 +108,23 @@ abstract class PT_Parse {
 		$this->skip_till( '{' );
 
 		while( $balancer < 0 && $this->next() ) {
-			if( $this->token == '{' )
+			if( $this->token() == '{' )
 				$balancer--;
-			else if( $this->token == '}' )
+			else if( $this->token() == '}' )
 				$balancer++;
 		}	
 	}
 }
 
 class PT_Parse_File extends PT_Parse {
+	private $classes;
+	private $functions;
+	private $constants;
+	private $globals;
+
+	public function __construct( $file ) {
+		$this->set( file_get_contents( $file ) );
+	}
 }
 
 class PT_Parse_Class extends PT_Parse {
@@ -114,3 +141,5 @@ class PT_Parse_Argument extends PT_Parse {
 
 class PT_Parse_Property extends PT_Parse {
 }
+
+//new PT_Parse_File( 'tests/class-wp-filesystem-direct.php' );
