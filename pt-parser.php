ef0ce24edcbb6cec25c69526ae5e6b1bf673eaa5
Kunal Bhalla <bhalla.kunal@gmail.com>
2011-09-21 18:48:57 +0530

    Switched to implement the iterator interface and added inline comments.

--- pt-parser.php
+++ pt-parser.php
@@ -1,15 +1,76 @@
 <?php
 
 /**
- * Base parser. Maintains state and utility functions.
+ * Classes for Parsing code. 
+ *
+ * Return parsed functions, classes, globals and constants.
+ *
+ * @version 0.2
+ * @author Kunal Bhalla
+ * @package PressTest
+ * @subPackage PT_Parser 
  */
-abstract class PT_Parse {
-	private static $tokens;
-	private static $position;
-	private static $tokens_length;
-	private static $mods;
-	private static $mod_distance;
-	protected static $mod_list = Array( 
+
+/**
+ * Value returned by the PT_Parse Iterator.
+ *
+ * @link PT_Parse. Returns details about the
+ * current token.
+ */ 
+class PT_Parse_Token {
+	public $value;
+	public $token;
+	public $line;
+	public $modifiers;
+
+	public function __construct( $args = Array() ) {
+		$value = array_key_exists( 'value', $args )? $args['value'] : "";
+		$token = array_key_exists( 'token', $args )? $args['token'] : "";
+		$line = array_key_exists( 'line', $args )? $args['line'] : "";
+		$modifiers = array_key_exists( 'modifiers', $args )? $args['modifiers'] : "";
+	}
+
+	public function get_modifier( $token ) {
+		if( isset( $this->modifiers[$token] ) )
+			return $this->modifiers[$token];
+		else return false;	
+	}
+}
+
+/**
+ * Smart iterator for tokens obtained parsed code.
+ */ 
+class PT_Parse implements Iterator {
+
+	/** 
+	 * Token list
+	 * @var Array()
+	 */
+	protected $tokens;
+
+	/**
+	 * Current Position
+	 * @var Int
+	 */
+	protected $position;
+
+	/**
+	 * Possible modifiers for upcoming tokens.
+	 * @var Array
+	 */
+	protected $mods;
+
+	/**
+	 * Distance since the modifier was captured.
+	 * @var Int
+	 */
+	protected $mod_distance;
+
+	/**
+	 * Tokens of modifiers to be captured.
+	 * @var Mixed
+	 */
+	protected $mod_list = Array( 
 		T_DOC_COMMENT,
 		T_FINAL,
 		T_PUBLIC,
@@ -19,91 +80,133 @@ abstract class PT_Parse {
 		T_STATIC
 	);	
 
-	private function __construct() {
-	}
-
-	protected function stop() {
-		$this->postition = $this->tokens_length();
-	}
-
-	protected function end() {
-		if( $this->position < $this->tokens_length ) 
-			return false;
-		return true;	
-	}
-
-	protected function reset() {
+	/**#@+
+	 * Function required for the Iterator interface.
+	 */
+	/**
+	 * Constructor -- tokenizes code.
+	 * @param String $data Code to be parsed.
+	 */
+	public function __construct( $data ) {
 		$this->position = 0;
-	}
-
-	protected function set( $tokens ) {
-		$this->postition = 0;
-		$this->tokens = $tokens;
-		$this->tokens_length = count( $tokens );
+		$this->tokens = token_get_all( $data );
 		$this->mods = Array();
 	}
 
-	private function modifiers() {
-		if( in_array( $this->token(), $this->mod_list ) ) {
-			$this->mods[ $this->token() ] = $this->value();
-			$this->mod_distance = 0;
-		} else if( $this->token() != T_WHITESPACE ) {
-			$this->mod_distance++;
-			if( $this->mod_distance > 0 )
-				$this->reset_modifiers();
-		}
+	/**
+	 * Checks whether the iterator can still move forward.
+	 */
+	public function valid() {
+		return isset( $this->tokens[ $this->position ] );
 	}
 
-	private function reset_modifiers() {
-		$this->mods = Array();
+	/**
+	 * Return to the start.
+	 *
+	 * Does nothing as the iterator should remember it's state across
+	 * foreach calls.
+	 */
+	public function rewind() {
 	}
 
-	protected function get_modifier( $token ) {
-		return $this->mods[ $this->token() ];
-	}
-
-	protected function each() {
-		$this->modifiers();
-	}
-
-	protected function next() {
+	/**
+	 * Moves to the next token and runs an 'each' internal call.
+	 */
+	public function next() {
 		$this->position++;
+
 		if( !$this->end() )
 			$this->each();
-		return !$this->end();
 	}
 
-	protected function token() {
-		if( $this->end() ) return false;
+	/**
+	 * Returns the current key value.
+	 */
+	public function key() {
+		if( $this->end() ) return NULL;
 
 		if( is_array( $this->tokens[ $this->position ] ) )
 			return $this->tokens[ $this->position ][ 0 ];
 		else return $this->tokens[ $this->position ];
 	}	
 
-	protected function value() {
-		if( $this->end() ) return false;
+	/**
+	 * Returns a PT_Parse_Token instance or NULL in case the iterator has overshot.
+	 * 
+	 * @return PT_Parse_Token|Null 
+	 */
+	public function current() {
+		if( $this->end() ) return NULL;
 
 		if( is_array( $this->tokens[ $this->position ] ) )
-			return $this->tokens[ $this->position ][ 1 ];
-		else return $this->tokens[ $this->position ];
-	}	
+			$value = $this->tokens[ $this->position ][ 1 ];
+		else $value = $this->tokens[ $this->position ];
 
+		return new PT_Parse_Token( Array( 
+			'token' => $this->key(),
+			'value' => $value(),
+			'modifiers' => $this->mods,
+			'line' => $this->line()
+		) );
+	}	
+	/**#@-*/
+
+	/**
+	 * Saves modifiers for future tokens.
+	 *
+	 * Saves modifiers from the mod_list, resets in case they 
+	 * become inapplicable.
+	 */
+	protected function modifiers() {
+		if( in_array( $this->key(), $this->mod_list ) ) {
+			$this->mods[ $this->key() ] = $this->value();
+			$this->mod_distance = 0;
+		} else if( $this->key() != T_WHITESPACE ) {
+			$this->mod_distance++;
+			if( $this->mod_distance > 0 )
+				$this->reset_modifiers();
+		}
+	}
+
+	/**
+	 * Resets all modifiers
+	 */
+	protected function reset_modifiers() {
+		$this->mods = Array();
+	}
+
+	/**
+	 * Run on all tokens internally. Useful in derived classes. 
+	 */
+	protected function each() {
+		$this->modifiers();
+	}
+
+	/**
+	 * Returns the line of the current token.
+	 */
 	protected function line() {
-		if( $this->end() ) return false;
+		if( $this->end() ) return NULL;
 
-		if( is_array( $this->tokens[ $this->position ] ) && isset( $this->tokens[ $this->position ][ 2 ] ) ) 
-			return $this->tokens[ $this->position ][ 2 ];
-	
-		return false;		
-	}	
+		if( is_array( $this->tokens[ $this->position ] ) && isset( $this->tokens[ $this->position ][ 2 ] ) )
+			return $this->tokens[ $this->position ][ 3 ];
+		else return NULL;
+	}
 
-	protected function skip_till( $token ) {
+	/**
+	 * Skip tokens till the specified token is reached.
+	 *
+	 * Useful for exploratory parsing
+	 */
+	public function skip_till( $token ) {
 		while( $this->token() != $token && !$this->end() )
 			$this->next();
 	}
 
-	protected function skip_block() {
+	/**
+	 * Find and skip the next { ... } block.
+	 */
+	public function skip_block() {
 		$balancer = -1;
 		$this->skip_till( '{' );
 
@@ -123,7 +226,13 @@ class PT_Parse_File extends PT_Parse {
 	private $globals;
 
 	public function __construct( $file ) {
-		$this->set( file_get_contents( $file ) );
+	}
+
+	public function getClasses() {
+		return $this->classes;
+	}
+
+	public function getFunctions() {
 	}
 }
 
@@ -142,4 +251,3 @@ class PT_Parse_Argument extends PT_Parse {
 class PT_Parse_Property extends PT_Parse {
 }
 
-//new PT_Parse_File( 'tests/class-wp-filesystem-direct.php' );
