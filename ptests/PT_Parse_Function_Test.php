2b859140eaaca98e00b84ab2864684c6a19d0be6
Kunal Bhalla <bhalla.kunal@gmail.com>
2011-09-29 17:47:42 +0530

    Completed basic test

--- ptests/PT_Parse_Function_Test.php
+++ ptests/PT_Parse_Function_Test.php
@@ -9,7 +9,7 @@
  * @subPackage ptests 
  */
 
-// include dirname( dirname( __FILE__ ) ) . "/pt-parser.php";
+include_once dirname( dirname( __FILE__ ) ) . "/pt-parser.php";
 
 class PT_Parse_Function_Test extends PHPUnit_Framework_TestCase {
 
@@ -29,8 +29,38 @@ class PT_Parse_Function_Test extends PHPUnit_Framework_TestCase {
 	 */
 	public function testSimple( $code ) {
 		$parser = new PT_Parser( $code );
-		$functionParser = new PT_Parse_Function( $parser );
+		$functions = Array();
+		$expected = Array(
+			"add_filter",
+			"has_filter",
+			"apply_filters",
+			"apply_filters_ref_array",
+			"remove_filter",
+			"remove_all_filters",
+			"current_filter",
+			"add_action",
+			"do_action",
+			"did_action",
+			"do_action_ref_array",
+			"has_action",
+			"remove_action",
+			"remove_all_actions",
+			"plugin_basename",
+			"plugin_dir_path",
+			"plugin_dir_url",
+			"register_activation_hook",
+			"register_deactivation_hook",
+			"register_uninstall_hook",
+			"_wp_call_all_hook",
+			"_wp_filter_build_unique_id"
+		);
 
-		$this->assertEquals( 'add_filter', $functionParser->get( 'name' ) );
+		foreach( $parser as $token ) {
+			if( $token->token == T_FUNCTION )
+				$functions[] = new PT_Parse_Function( $parser );
+		}
+
+		foreach( $expected as $key => $check )
+			$this->assertEquals( $check, $functions[ $key ]->get( "name" ) );
 	}
 }
